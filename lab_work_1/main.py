17f9ff2e1a5279d035a7968280b6c00b36fadc26
Iryna Perkhun <iryna.perkhun@gmail.com>
2023-09-20 10:59:19 +0300

    checking operators, calculation, repeating calculation are done(task 2, 3, 4)

--- lab_work_1/main.py
+++ lab_work_1/main.py
@@ -1,5 +1,6 @@
-# Function to check if a data can be converted to a float
-def is_valid_float(data):
+while True:
+# Function to check if a data can be converted to a float. If data can we convert it to float
+  def is_valid_float(data):
     try:
         float(data)
         return True
@@ -7,7 +8,7 @@ def is_valid_float(data):
         return False
 
 # Prompt the user to enter the first number
-while True:
+  while True:
     num1_input = input("Enter the first number: ")
     if is_valid_float(num1_input):
         number1 = float(num1_input)
@@ -16,7 +17,7 @@ while True:
         print("Invalid input. Please enter a valid number.")
 
 # Prompt the user to enter the second number
-while True:
+  while True:
     num2_input = input("Enter the second number: ")
     if is_valid_float(num2_input):
         number2 = float(num2_input)
@@ -25,27 +26,35 @@ while True:
         print("Invalid input. Please enter a valid number.")
 
 # Prompt the user to enter an operator
-valid_operators = ['+', '-', '*', '/']
-while True:
+  valid_operators = ['+', '-', '*', '/']
+  while True:
     operator = input("Enter the operator (+, -, *, /): ")
     if operator in valid_operators:
         break
     else:
         print("Invalid operator. Please enter a valid operator from the list (+, -, *, /).")
 
-# Perform calculations based on the entered operator
-if operator == '+':
+# Perform calculations based on the operator which user entered
+  if operator == '+':
     result = number1 + number2
-elif operator == '-':
+  elif operator == '-':
     result = number1 - number2
-elif operator == '*':
+  elif operator == '*':
     result = number1 * number2
-elif operator == '/':
+  elif operator == '/':
     # Check for division by zero
     if number2 == 0:
         result = "Error: Division by zero is not allowed"
     else:
-        result = number1 / number2
+                result = number1 / number2
+   
 
 # Display the result
-print("Result:", result)
+  print("Result:", result)
+
+#Ask user if he wants to perform one more calculation
+  another_calculation = input("Do you want to perform another calculation? (yes/no): ")
+  if another_calculation.lower() != 'yes':
+        #exit if user doesn't want one more calculation
+        break
+ 
